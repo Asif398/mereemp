2c25cd796a429564df1bf5f9a52aab20ac56d121
Asif Raza <asif.raza@gmail.com>
2025-02-19 00:12:09 +0530

    is bar humlog repository layer me update kiyae haii interface

--- src/main/java/com/employeework/repostiory/EmployeeRepository.java
+++ src/main/java/com/employeework/repostiory/EmployeeRepository.java
@@ -4,4 +4,6 @@ import com.employeework.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+//    this is the marker interface which means this is the empty interface
 }
